2f6a35983e0f4708953ab078e6758bf502fbad6a
Nik Zandian <nik@quietrocket.com>
2020-05-27 00:57:01 -0700

    Added message editing

--- src/agents/discord.ts
+++ src/agents/discord.ts
@@ -1,6 +1,8 @@
 import {
     Client,
     MessageOptions,
+    Message,
+    PartialMessage,
     TextChannel
 } from 'discord.js';
 
@@ -16,12 +18,106 @@ export default class DiscordAgent extends LatexAgent {
     private token: string;
     private client: Client;
     private targets: DiscordTargets;
+    private pruneInterval: number;
+    private messageMap: Map<string, Message>;
+    private timeout: ReturnType<typeof setTimeout> | null;
 
-    constructor(token: string, targets: DiscordTargets) {
+    constructor(token: string, targets: DiscordTargets, pruneInterval: number) {
         super();
         this.token = token;
         this.client = new Client();
         this.targets = targets;
+        this.pruneInterval = pruneInterval;
+        this.messageMap = new Map();
+        this.timeout = null;
+    }
+
+    private pruneMap() {
+        if (this.messageMap.size === 0)
+            return;
+
+        const now = Date.now();
+
+        Array.from(this.messageMap.entries())
+            .filter((value) => {
+                const delta = now - value[1].createdTimestamp;
+                return delta / 1000 >= 60 * this.pruneInterval;
+            })
+            .forEach((value) => {
+                this.messageMap.delete(value[0]);
+            });
+        
+        if (this.messageMap.size === 0) {
+            this.timeout = null;
+            return;
+        }
+        
+        const oldest = Math.min.apply(null, Array.from(this.messageMap.values()).map((message) => message.createdTimestamp));
+        const next = 60 * this.pruneInterval - (now - oldest) / 1000
+        
+        this.timeout = setTimeout(() => this.pruneMap(), 1000 * next);
+    }
+
+    private trackMessage(id: string, message: Message) {
+        this.messageMap.set(id, message);
+        if (this.timeout === null)
+            this.timeout = setTimeout(() => this.pruneMap(), 1000 * 60 * this.pruneInterval);
+    }
+
+    private async handleMessage(content: string): Promise<MessageOptions | string | null> {
+        let reply: MessageOptions | string | null = null;
+
+        const match = content.match(/\.tex (.*)/);
+        if (!match)
+            return reply;
+
+        try {
+            const result = await this.render(match[1]);
+            reply = {
+                files: [
+                    result
+                ]
+            };
+        } catch (e) {
+            if (e instanceof ParseError)
+                reply = '```\n' + e.message + '\n```';
+            else
+                console.error(e);
+        } finally {
+            return reply;
+        }
+    }
+
+    private shouldIgnore(msg: Message | PartialMessage): boolean {
+        if (
+            !(msg.channel instanceof TextChannel) ||
+            msg.guild === null ||
+            !this.targets.hasOwnProperty(msg.guild.id) ||
+            msg.author === null ||
+            this.client.user === null ||
+            msg.author.id === this.client.user.id
+        )
+            return true;
+
+        const guild = this.targets[msg.guild.id];
+        if (guild.indexOf(msg.channel.id) === -1)
+            return true;
+
+        return false;
+    }
+
+    private async sendResult(msg: Message | PartialMessage) {
+        if (msg.content === null)
+            return;
+
+        const result = await this.handleMessage(msg.content);
+
+        if (result === null)
+            return;
+
+        const reply = await msg.channel.send(result);
+
+        this.trackMessage(msg.id, reply);
     }
 
     async start() {
@@ -30,38 +126,30 @@ export default class DiscordAgent extends LatexAgent {
 
         this.client
             .on('message', async (msg) => {
-                if (!(msg.channel instanceof TextChannel) || msg.guild === null)
+                if (this.shouldIgnore(msg))
                     return;
 
-                if (!this.targets.hasOwnProperty(msg.guild.id))
+                await this.sendResult(msg);
+            })
+            .on('messageUpdate', async (_, msg) => {
+                if (!this.messageMap.has(msg.id) || msg.content === null)
+                    return;
+                
+                await this.messageMap.get(msg.id)!.delete();
+
+                await this.sendResult(msg);
+            })
+            .on('messageDelete', async (msg) => {
+                const id = msg.id;
+                if (!this.messageMap.has(id))
                     return;
 
-                const guild = this.targets[msg.guild.id] as string[];
-                if (guild.indexOf(msg.channel.id) === -1)
-                    return;
-
-                const match = msg.content.match(/\.tex (.*)/);
-                if (!match)
-                    return;
-
-                try {
-                    const result = await this.render(match[1]);
-                    const options: MessageOptions = {
-                        files: [
-                            result
-                        ]
-                    }
-                    msg.reply(options);
-                } catch (e) {
-                    if (e instanceof ParseError)
-                        msg.reply('```\n' + e.message + '\n```');
-                    else
-                        console.error(e);
-                }
+                await this.messageMap.get(id)!.delete();
+                this.messageMap.delete(id);
             })
             .on('disconnect', async () => {
                 await this.destroy();
-                await this.client.destroy();
+                this.client.destroy();
             })
 
         this.client.login(this.token);
