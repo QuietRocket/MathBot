91904c3921ba8d61120d86d821677a388ad300ab
Nik Zandian <nik@quietrocket.com>
2020-05-27 00:59:52 -0700

    Added configuration file

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import { Configuration } from './index';
+
+export const config: Configuration = {
+    // Discord bot token
+    token: 'insert token here',
+    // A map of all targets the bot should listen to
+    targets: {
+        // Guild ID
+        'insert guild id here': [
+            // A list of channel ID's
+            'insert channel id here',
+            'and here...'
+        ]
+    },
+    // How often edit history should be pruned in minutes
+    pruneInterval: 10
+}
